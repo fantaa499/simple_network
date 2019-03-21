74725a26d3dfd8fb2c19ee0ff1c35724b5d35418
Anton Golubev <fantaa499@gmail.com>
2019-03-21 15:24:51 +0300

    activation functions is Layer. Add weights initializator class. Del activation_function.py

--- net/layer.py
+++ net/layer.py
@@ -1,30 +1,93 @@
-class Layer:
-    def __init__(self, n_neurons, type, f_activation=None):
+import numpy as np
+from abc import abstractmethod, ABC
+
+
+# Абстрактный метод слоя
+class Layer(ABC):
+    def __init__(self):
+        pass
+
+    # Обязательный метод для прямого прохода
+    @abstractmethod
+    def forward(self, signal):
+        pass
+
+    # Обязательный метод для обратного прохода
+    @abstractmethod
+    def back(self, signal):
+        pass
+
+    # Обязательный метод для легкой инициализации матриц весов
+    @abstractmethod
+    def has_neurons(self):
+        pass
+
+
+# Входной слой
+class Input(Layer):
+    def __init__(self, n_neurons):
         self.n_neurons = n_neurons
-        try:
-            if type == "in":
-                self.type = "in"
-            elif type == "out":
-                self.type = "out"
-            elif type == "hidden":
-                self.type = "hidden"
-                if f_activation is None:
-                    raise(HiddenLayerException)
-            else:
-                raise(LayerTypeException)
-        except LayerTypeException:
-            print("Введите корректный тип слоя, in, out или hidden")
-        except HiddenLayerException:
-            print("Укажите тип функции активации")
 
-        self.f_activation = f_activation
+    def forward(self, signal):
+        return signal
+
+    def back(self, signal):
+        return signal
+
+    def has_neurons(self):
+        return True
 
 
-class HiddenLayerException(Exception):
-    def __init__(self):
-        Exception.__init__(self)
+# Полносвязный слой
+class Dense(Layer):
+    def __init__(self, n_neurons):
+        self.n_neurons = n_neurons
+        self._weights = []
+
+    def forward(self, signal):
+        return np.dot(self.weights, signal)
+
+    def back(self, signal):
+        return np.dot(signal, self.weights.T)
+
+    def has_neurons(self):
+        return True
+
+    @property
+    def weights(self):
+        return self._weights
+
+    @weights.setter
+    def weights(self, weights):
+        self._weights = weights
+
+    @weights.deleter
+    def weights(self):
+        del self.weights
 
 
-class LayerTypeException(Exception):
-    def __init__(self):
-        Exception.__init__(self)
+# Функции активации:
+class ReLu(Layer):
+    def forward(self, signal):
+        f = signal if signal > 0 else 0
+        return f
+
+    def back(self, signal):
+        f = 1 if signal > 0 else 0
+        return f
+
+    def has_neurons(self):
+        return False
+
+
+class Sigmoid(Layer):
+    def forward(self, signal):
+        f = 1 / (1 + np.exp(-signal))
+        return f
+
+    def back(self, signal):
+        f = self.forward(signal) * (1 - self.forward(signal))
+        return f
+
+    def has_neurons(self):
+        return False
