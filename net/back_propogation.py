0ed4bbfae2026aa5ea1fa96c797cc0bfc5ad82be
tdtce <fantaa499@gmail.com>
2019-03-23 13:35:43 +0300

    add batch separation

--- net/back_propogation.py
+++ net/back_propogation.py
@@ -1,10 +1,13 @@
+from random import randint
+import numpy as np
 
 
 class BackPropagation:
-    def __init__(self, network, l_rate=1, l_rate_decay=0.1):
+    def __init__(self, network, l_rate=1, l_rate_decay=0.1, l_rate_decay_n_epoch=20):
         self.net = network
         self.l_rate = l_rate
-        self.l_rate_decay = l_rate_decay
+        self._l_rate_decay = l_rate_decay
+        self._l_rate_decay_n_epoch = l_rate_decay_n_epoch
         # Вспомогательная переменная в которой будут храниться
         # промежуточные значения во время обучения
         self.current_signals = []
@@ -12,16 +15,17 @@ class BackPropagation:
         # номер эпохи
         self.i_epoch = 0
 
-    def fit(self, X, y, n_epoch):
+    def fit(self, X, y, n_epoch, batch_size = 100):
         for i in range(n_epoch):
             self.i_epoch = i
-            self._forward_propagation(X)
-            self._save_loss(y)
-            self._back_propagation(y)
+            batch_x, batch_y = self._gen_batch(X, y)
+            self._forward_propagation(batch_x)
+            self._save_loss(batch_y)
+            self._back_propagation(batch_y)
             if i % 100 == 0:
                 print(f"Epoch {i}: loss = {self.current_signals[-1]}\n")
-            if i % 20:
-                self.l_rate *= self.l_rate_decay
+            if i % self._l_rate_decay_n_epoch:
+                self._l_rate_reduce()
 
     def _forward_propagation(self, X):
         temp_in = X
@@ -43,6 +47,7 @@ class BackPropagation:
         for i in reversed(l_indexes):
             # layer указывает на тоже место в памяти, что и self.net.layers[i]
             layer = self.net.layers[i]
+            # Это условие выполняется в слоях, где требуется обновление весов
             if self.net.layers[i].has_neurons():
                 dw, db = layer.back(ds)
                 # dlr попровка для оптимизации learning rate
@@ -53,9 +58,22 @@ class BackPropagation:
             else:
                 ds *= layer.back(self.current_signals[i])
 
+    def _gen_batch(self, X, y, batch_size=100):
+        batch_x = np.array([])
+        batch_y = np.array([])
+        n_samples = len(X)
+        for _ in range(batch_size):
+            i = randint(0, n_samples - 1)
+            batch_x = np.append(batch_x, X[i])
+            batch_y = np.append(batch_y, y[i])
+        return batch_x, batch_y
+
     def _save_loss(self):
         loss = self.current_signals[-1]
         self.losses.append(loss)
+
+    def _l_rate_reduce(self):
+        self.l_rate *= self._l_rate_decay
 
     def predict(self, X):
         self._forward_propagation(X)
